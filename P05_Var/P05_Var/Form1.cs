df0fb1e9f2ca9f5264e82adc6b2fd50495026ba4
Levente Németh <NemethLevente@hotmail.hu>
2020-11-07 15:32:00 +0100

    Commit 5.0

--- P05_Var/P05_Var/Form1.cs
+++ P05_Var/P05_Var/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using P05_Var.Entities;
 
 namespace P05_Var
 {
@@ -15,13 +16,26 @@ namespace P05_Var
         List<Tick> Ticks;
         PortfolioEntities context = new PortfolioEntities();
 
+        List<PortfolioItem> Portfolios = new List<PortfolioItem>();
+
         public Form1()
         {
 
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+            
 
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolios.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolios.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolios.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolios;
         }
 
         private void Form1_Load(object sender, EventArgs e)
