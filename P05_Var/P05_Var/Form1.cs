f9dce81db0d2b15ae3637e287617fef89d917242
Levente Németh <NemethLevente@hotmail.hu>
2020-11-07 15:20:42 +0100

    Commit 4.0

--- P05_Var/P05_Var/Form1.cs
+++ P05_Var/P05_Var/Form1.cs
@@ -12,9 +12,21 @@ namespace P05_Var
 {
     public partial class Form1 : Form
     {
+        List<Tick> Ticks;
+        PortfolioEntities context = new PortfolioEntities();
+
         public Form1()
         {
+
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
