d8cb183cd02f4cfb2a010d7acae68566c32f8a90
Levente Németh <NemethLevente@hotmail.hu>
2020-11-07 12:59:10 +0100

    Commit utolsó

--- P04_Excel/P04_Excel/Form1.cs
+++ P04_Excel/P04_Excel/Form1.cs
@@ -27,6 +27,50 @@ namespace P04_Excel
             InitializeComponent();
             LoadData();
             CreateExcel();
+            FormatTable();
+        }
+
+        private void FormatTable()
+        {
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, 9));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            int lastRowID = xlSheet.UsedRange.Rows.Count;
+
+            Excel.Range TableRange = xlSheet.get_Range(GetCell(1, 1), GetCell(lastRowID, 9));
+            TableRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            Excel.Range First = xlSheet.get_Range(GetCell(2, 1), GetCell(lastRowID, 1));
+            First.Font.Bold = true;
+            First.Interior.Color = Color.LightYellow;
+
+            Excel.Range Last = xlSheet.get_Range(GetCell(2, 9), GetCell(lastRowID,9));
+            Last.Interior.Color = Color.LightGreen;
+
+
+
+            string GetCell(int x, int y)
+            {
+                string ExcelCoordinate = "";
+                int dividend = y;
+                int modulo;
+
+                while (dividend > 0)
+                {
+                    modulo = (dividend - 1) % 26;
+                    ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                    dividend = (int)((dividend - modulo) / 26);
+                }
+                ExcelCoordinate += x.ToString();
+
+                return ExcelCoordinate;
+            }
         }
 
         private void CreateExcel()
@@ -79,6 +123,51 @@ namespace P04_Excel
             {
                 xlSheet.Cells[1, i+1] = headers[i];
             }
+
+            object[,] values = new object[flats.Count, headers.Length];
+
+            int c = 0;
+
+            foreach (Flat a in flats)
+            {
+                values[c, 0] = a.Code;
+                values[c, 1] = a.Vendor;
+                values[c, 2] = a.Side;
+                values[c, 3] = a.District;
+                values[c, 4] = a.Elevator;
+                values[c, 5] = a.NumberOfRooms;
+                values[c, 6] = a.FloorArea;
+                values[c, 7] = a.Price;
+
+                values[c, 8] = "=" + GetCell(c+2, 8)+"/"+GetCell(c+2, 7);
+
+
+
+                c++;
+            }
+
+            string GetCell(int x, int y)
+            {
+                string ExcelCoordinate = "";
+                int dividend = y;
+                int modulo;
+
+                while (dividend > 0)
+                {
+                    modulo = (dividend - 1) % 26;
+                    ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                    dividend = (int)((dividend - modulo) / 26);
+                }
+                ExcelCoordinate += x.ToString();
+
+                return ExcelCoordinate;
+            }
+
+            xlSheet.get_Range(
+             GetCell(2, 1),
+             GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+
         }
 
         private void LoadData()
