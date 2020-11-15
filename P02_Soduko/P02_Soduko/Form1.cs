8bf4f6b84ff5a1f1444fcde7c3d664ed0b159e91
Levente Németh <NemethLevente@hotmail.hu>
2020-11-15 18:20:21 +0100

    Commit 5.0

--- P02_Soduko/P02_Soduko/Form1.cs
+++ P02_Soduko/P02_Soduko/Form1.cs
@@ -14,7 +14,41 @@ namespace P02_Soduko
     {
         public Form1()
         {
+            CratePlayField();
+            LoadSusokus();
             InitializeComponent();
+            GetRandomQuize();
+            NewGame();
+        }
+
+        private void NewGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void GetRandomQuize()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadSusokus()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CratePlayField()
+        {
+            int lineWidth = 5;
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; i < 9; j++)
+                {
+                    SudokuField s = new SudokuField();
+                    s.Top = i * 30 + (int)(Math.Floor((double)(i / 3))) * lineWidth;
+                    s.Left = j * 30 + (int)(Math.Floor((double)(j / 3))) * lineWidth;
+                    panel1.Controls.Add(s);
+                }
+            }
         }
     }
 }
